908bd9066f2c0b954ec78b55624c39fc8fa3415f
Shuhei.Nakano <nukano0522@gmail.com>
2025-01-22 11:29:26 +0900

    vite.config.jsを修正

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,18 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: 3000,
-    watch: {
-      usePolling: true,
-      interval: 1000,
-    },
     proxy: {
       '/api': {
         target: 'http://backend:8000',
         changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/api')
       }
     }
   }
